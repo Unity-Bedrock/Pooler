63b74af5024009f9fe38be95091710d8f9f055c9
Ferid <ferid.dragon@hotmail.com>
2020-12-27 21:59:12 +0100

    Added unit tests for the BaseGameObjectDisposer class.

---
+++ Assets/Pooler/Tests/Runtime/BaseGameObjectDisposerTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Com.UnityBedrock.Pooler.GameObjects.Tests
+{
+    [TestFixture]
+    public class BaseGameObjectDisposerTest
+    {
+        private BaseGameObjectDisposer _baseGameObjectDisposer;
+
+        private Transform _parent;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _parent = new GameObject().transform;
+            _baseGameObjectDisposer = new BaseGameObjectDisposer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_parent.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator Dispose_DestroysGameObject()
+        {
+            GameObject newGameObject = new GameObject();
+            newGameObject.transform.SetParent(_parent);
+            Assert.AreEqual(1, _parent.childCount);
+
+            _baseGameObjectDisposer.Dispose(newGameObject);
+            yield return null;
+
+            Assert.AreEqual(0, _parent.childCount);
+        }
+    }
+}
